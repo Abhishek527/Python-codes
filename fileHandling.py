855cd5e331084c15b1b86465e0fd84d8ed304cfd
Abhishek P <abhishekp.abhi84@gmail.com>
2025-05-19 16:39:42 +0530

    code

---
+++ fileHandling.py
@@ -0,0 +1,12 @@
+with open("Demo.txt","r") as f1:
+     data = f1.read()
+     print(data)
+
+#f2 = open("Demo2.txt","w")
+#f2.write("Added new text")
+#f2.close()
+
+#f3 = open("Demo2.txt","a")
+#f3.write("\nappended text")
+#f3.close()
+
